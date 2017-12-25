cccdcd40d0ed9420edadb2810b7b2a51b31c6e04
dale426 <914397365@qq.com>
2017-12-25 23:17:58 +0800

    children

--- src/module/props/Props.vue
+++ src/module/props/Props.vue
@@ -3,23 +3,37 @@
         <div class="parents">
             <h2>我是父组件</h2>
             <div class="p-content">
-                <div class="u-text">父组件内容： {{msg}}</div>
+                <div class="u-text">父组件内容</div>
+                <Form :model="formItem" :label-width="80">
+                    <FormItem label="姓名：">
+                        <Input v-model="formItem.name" placeholder="Enter something..."> </Input>
+                    </FormItem>
+                </Form>
                 <p class="u-info">点击按钮激活子组件，并传值下去</p>
                 <Button type="warning">激活子组件</Button>
             </div>
+        </div>
+        <div>
+            <Children :name="formItem.name" :age="formItem.age" :money="formItem.money"> </Children>
         </div>
     </contenerWrap>
 </template>
 <script>
 import contenerWrap from '@/components/header/contenerWrap'
+import Children from './components/children'
 export default {
     name: 'Props',
     data () {
         return {
-            msg: 0
+            msg: 0,
+            formItem: {
+                name: 'liyulong',
+                age: '25',
+                money: 1000000
+            }
         }
     },
-    components: {contenerWrap}
+    components: {contenerWrap, Children}
 }
 </script>
 <style lang="less">
---
+++ src/module/props/components/children.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>我是子组件</h1>
+        <div>值由父组件传递下来：</div>
+        <div>
+            <Button type="success">接收</Button>
+            <ul>
+                <li>姓名：{{ name }}</li>
+                <li>年龄：{{ age }} </li>
+                <li>存款：{{ money }} </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'chilrdren',
+    props: ['name', 'age', 'money'],
+    data() {
+        return {
+            message: ''
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="less">
+
+</style>
