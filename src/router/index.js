f81d7d3ef1aff21f09694dbe809a61453073f684
yulong <yulongli>
2018-06-28 21:36:52 +0800

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-<<<<<<< HEAD
     routes: [
         {
             path: '/',
@@ -26,73 +25,31 @@ export default new Router({
                     path: '/home',
                     name: 'Home',
                     component: r => require.ensure([], () => r(require('../module/home/Home.vue')), 'Home')
-                },
-                {
+                }, {
                     path: '/props',
                     name: 'props',
                     component: r => require.ensure([], () => r(require('../module/props/Props.vue')), 'Props')
-                },
-                {
+                }, {
+                    path: '/zoom',
+                    name: 'ImgZoom',
+                    component: r => require.ensure([], () => r(require('../module/other/Img-zoom.vue')), 'ImgZoom')
+                }, {
                     path: '/test',
                     name: 'test',
                     component: r => require.ensure([], () => r(require('../module/demo/Test.vue')), 'Test')
-                },
-                {
-                    path: '/fundhold',
+                }, {
+                    path: '/fund',
+                    name: 'fundIndex',
+                    component: r => require.ensure([], () => r(require('../module/fund/index.vue')), 'FundIndex')
+                }, {
+                    path: '/fund/hold',
                     name: 'fundHolding',
-                    component: r => require.ensure([], () => r(require('../module/fund/Fund-holding.vue')), 'Test')
-                },
-                {
-                    path: '/preimg',
-                    name: 'preimg',
-                    component: r => require.ensure([], () => r(require('../module/demo/ImageLoad.vue')), 'Test')
-                }
-            ]
-        }
-
-       /*
-        {
-=======
-    routes: [{
-        path: '/',
-        // name: 'Main',
-        component: r => require.ensure([], () => r(require('../components/main/Main.vue')), 'Main'),
-        children: [{
-            path: '',
-            redirect: '/long'
-        }, {
-            path: '/long',
-            name: 'long',
-            component: r => require.ensure([], () => r(require('../module/home/Long.vue')), 'Long')
-        }, {
->>>>>>> feature1.0
-            path: '/home',
-            name: 'Home',
-            component: r => require.ensure([], () => r(require('../module/home/Home.vue')), 'Home')
-        }, {
-            path: '/props',
-            name: 'props',
-            component: r => require.ensure([], () => r(require('../module/props/Props.vue')), 'Props')
-        }, {
-            path: '/zoom',
-            name: 'ImgZoom',
-            component: r => require.ensure([], () => r(require('../module/other/Img-zoom.vue')), 'ImgZoom')
-        }, {
-            path: '/test',
-            name: 'test',
-            component: r => require.ensure([], () => r(require('../module/demo/Test.vue')), 'Test')
-        }, {
-            path: '/fund',
-            name: 'fundIndex',
-            component: r => require.ensure([], () => r(require('../module/fund/index.vue')), 'FundIndex')
-        }, {
-            path: '/fund/hold',
-            name: 'fundHolding',
-            component: r => require.ensure([], () => r(require('../module/fund/Fund-holding.vue')), 'Hold')
-        }, {
-            path: '/fund/select',
-            name: 'selfSelect',
-            component: r => require.ensure([], () => r(require('../module/fund/Fund-select.vue')), 'Selectself')
+                    component: r => require.ensure([], () => r(require('../module/fund/Fund-holding.vue')), 'Hold')
+                }, {
+                    path: '/fund/select',
+                    name: 'selfSelect',
+                    component: r => require.ensure([], () => r(require('../module/fund/Fund-select.vue')), 'Selectself')
+                }]
         }]
-    }]
 })
+
